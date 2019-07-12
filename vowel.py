36ace71eb21f97dd4eaaa9c93a956ba0858dafae
BEEMRAO <52704334+BEEMRAO@users.noreply.github.com>
2019-07-12 15:57:21 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -2,9 +2,9 @@ x=input()
 a=['a','e','i','o','u','A','E','I','O','U']
 if ((x>='a' and x<='z') or (x>='A' and x<='Z')):
   if x in a:
-    print("vowel")
+    print("Vowel")
   else:
-    print("consonant")
+    print("Consonant")
   else:
     print("invalid")
    
