009a883d1a3cfd501189fd5a12c9240be95b9abb
BEEMRAO <52704334+BEEMRAO@users.noreply.github.com>
2019-07-12 15:36:15 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int factorial=1;
+  int x,i;
+    scanf("%d",&x);
+  if(x<20)
+  {
+    for(i=1;i<=x;i++)
+  {
+    factorial=factorial*i;
+  }
+  print("%d",factorial);
+  }
+  }
